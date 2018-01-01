3602aad5cdff1d383b9380b14ac483e825b54197
DhruvD <dhruv.datt@tv2consulting.com>
2018-01-01 09:55:48 +0530

    integration socket.io with d3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require('path');
 var readline = require('readline');
 var stream = require('stream');
 
-server.listen(80,()=>{
+server.listen(3030,()=>{
     console.log('Server listening on port 80')
 });
 
@@ -16,11 +16,10 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
+    // socket.emit('news', { hello: 'world' });
     socket.on('readFile', function (data) {
         var extension = data.filename.split(".").pop();
         if(extension != 'txt') return socket.emit('errorReading', { err: "Can read only .txt files" });
-        console.log('test execution');
 
         var filePath = path.join(__dirname, data.filename)
         var instream = fs.createReadStream(filePath);
@@ -29,7 +28,6 @@ io.on('connection', function (socket) {
 
         var totalCount = 0;
         var perCount = 100;
-        var string = "Lorem";
         var resultData = {};
         var wordCount = 0;
 
@@ -38,20 +36,23 @@ io.on('connection', function (socket) {
             for(var i = 0; i < words.length ; i++){
                 totalCount++;
                 
-                if(words[i] == string) wordCount++;
+                if(words[i] == data.searchString) wordCount++;
                 if(totalCount % perCount==0){
                     resultData = Object.assign(resultData,{
                         [totalCount]: wordCount
+                    });
+                    socket.emit('searchResult', { 
+                        [totalCount]: wordCount 
                     });
                     wordCount=0;
                 }
             }
 
-            socket.emit('searchResult', { searchResult: resultData });
+            // socket.emit('searchResult', { searchResult: resultData });
         });
                 
         rl.on('close', function() {
-            // console.log(resultData)
+            console.log(resultData)
             socket.emit('finishedReading', { readingFinished: true });
         });
         
--- src/components/line-chart.js
+++ src/components/line-chart.js
@@ -5,55 +5,63 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      temp: [{
-        'sale': '202',
-        'year': '2000'
-      }, {
-        'sale': '215',
-        'year': '2001'
-      }, {
-        'sale': '179',
-        'year': '2002'
-      }, {
-        'sale': '199',
-        'year': '2003'
-      }, {
-        'sale': '134',
-        'year': '2003'
-      }, {
-        'sale': '176',
-        'year': '2010'
-      }],
-      line: ''
+      chartData: [],
+      fileName: 'sample.txt',
+      search: 'Lorem'
     }
   },
-  mounted () {
-    console.log(this.$refs.visualisation)
-    const vis = d3.select(this.$refs.visualisation)
-    const WIDTH = 1000
-    const HEIGHT = 500
-    const MARGINS = {
-      top: 20,
-      right: 20,
-      bottom: 20,
-      left: 50
-    }
-    const xScale = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000, 2010])
-    const yScale = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134, 215])
-    const xAxis = d3.axisBottom(xScale)
-    const yAxis = d3.axisLeft(yScale)
+  methods: {
+    createLineChart (data) {
+      this.chartData = []
+      // var test = data[Object.keys(data)[Object.keys(data).length - 1]]
+      // console.log(test)
+      this.chartData.push({
+        char: Object.keys(data)[0],
+        occ: data[Object.keys(data)[0]]
+      })
 
-    vis.append('svg:g').attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')').call(xAxis)
-    vis.append('svg:g').attr('transform', 'translate(' + (MARGINS.left) + ',0)').call(yAxis)
-    let lineGen = d3.line()
+      var minY = 0
+      var maxY = 0
+      // console.log('this.temp', this.temp)
+      var myNode = document.getElementById('visualisation')
+      while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild)
+      }
+      const vis = d3.select(this.$refs.visualisation)
+      const WIDTH = 1000
+      const HEIGHT = 500
+      const MARGINS = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 50
+      }
+      const xScale = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([this.chartData[0].occ, this.chartData[this.chartData.length - 1].occ])
+      const yScale = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([ Math.min(...Object.values(data)), Math.max(...Object.values(data))])
+      // const xScale = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([Object.keys(data)[0], Object.keys(data)[Object.keys(data).length - 1]])
+      // const yScale = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([ Math.min(...Object.values(data)), Math.max(...Object.values(data))])
+      const xAxis = d3.axisBottom(xScale)
+      const yAxis = d3.axisLeft(yScale)
+
+      vis.append('svg:g').attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')').call(xAxis)
+      vis.append('svg:g').attr('transform', 'translate(' + (MARGINS.left) + ',0)').call(yAxis)
+      let lineGen = d3.line()
       .x(function (d) {
-        console.log(d)
-        console.log(xScale(d.year))
-        return xScale(d.year)
+        return xScale(d.char)
       })
       .y(function (d) {
-        return yScale(d.sale)
+        return yScale(d.occ)
       })
-    vis.append('svg:path').attr('d', lineGen(this.temp)).attr('stroke', 'green').attr('stroke-width', 2).attr('fill', 'none')
+      vis.append('svg:path').attr('d', lineGen(this.chartData)).attr('stroke', 'green').attr('stroke-width', 2).attr('fill', 'none')
+    },
+    searchString () {
+      var self = this
+      if (global.socket) {
+        global.socket.on('searchResult', function (data) {
+          self.createLineChart(data)
+        })
+        global.socket.emit('readFile', { filename: this.fileName, searchString: this.search })
+      }
+    }
   }
 }
